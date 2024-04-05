f7b84859525501d31c27ad7d2370a46ed13cab7f
ArthurFraga27 <161150252+ArthurFraga27@users.noreply.github.com>
2024-04-05 02:49:45 -0300

    contact att
    atualização na área de contato

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,14 @@ const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+function avisar(){
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+    const caso = document.getElementById('caso').value;
+    if(nome === '' || telefone === '' || caso === ''){
+        window.alert('Por Gentileza, preencha os Dados corretamente!')
+    }else{
+        window.alert('Entraremos em contato o quanto antes')
+    }
+}
